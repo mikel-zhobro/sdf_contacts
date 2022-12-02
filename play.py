acbb8cadc8ec017edc2d92744b970147d5440628
Mikel Zhobro <zhobromikel@gmail.com>
2022-12-02 18:16:29 +0100

    add  extra playground

--- play.py
+++ play.py
@@ -1,10 +1,11 @@
 from sdf.d2 import circle
+from sdf import *
 
 
 
+f = (circle(0.3, [0.5, 0.5]) | circle(0.3, [0.2, 0.2])).extrude(0.1) | circle(0.2, [1.2, 1.2]).extrude(0.1)
 
-f = (circle(0.3, [0.5, 0.5]) | circle(0.3, [0.2, 0.2]))#.extrude(0.1) | circle(0.2, [1.2, 1.2]).extrude(0.1)
 # f.plot(samples=1000)
-f.save('play.obj')
-# f.plot()
+f.save('outputs/play.obj')
+# f.save()
 # f.show_slice(z=0)
